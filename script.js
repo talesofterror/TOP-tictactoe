8e7f9c1bc59491ecce9ad232ff4e30903dc94d93
mike <mich.rivera84@gmail.com>
2025-01-22 21:57:19 -0500

    engine error function

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 
-function Game () {
-	let score = 0
-	let turn = true
-	// const placements = new Array(3).fill(new Array(3)) 
-	// ^ not working as expected
-	const placements = [["", "", ""], ["", "", ""], ["", "", ""]]
-
-	return {score, turn, placements}
-}
-
-function Player (gamepiece, game, board) {
-	const Move = (x, y) => {
-		game.placements[x][y] = gamepiece
-		board.Update()
-	}
-
-	return {gamepiece, game, board, Move}
-}
-
 const Engine = (function () {
 	let gameArchive = []
 	let currentGame = gameArchive[gameArchive.length-1]
@@ -31,16 +12,48 @@ const Engine = (function () {
 		const board = new Board(game)
 		return {
 			game,
-			// game: new Game(),
+			// game: new Game(), // why doesn't this work?
 			board,
-			// board: new Board(this.game),
+			// board: new Board(game), // why doesn't this work?
 			player : new Player(playerSigil, game, board),
 			opponent: new Player(oppSigil, game, board),
 		}
 	}
 
-	return {gameArchive, currentGame, turn, Evaluate, CreateGame}
+	const Error = (errorType) => {
+		switch (errorType) {
+			case "move":
+				console.log("Invalid move")
+		}
+	}
+
+	return {
+		gameArchive, currentGame, turn, 
+		Evaluate, CreateGame, Error}
 })()
+
+function Game () {
+	let score = 0
+	let turn = true
+	// const placements = new Array(3).fill(new Array(3)) 
+	// ^ not working as expected
+	const placements = [["", "", ""], ["", "", ""], ["", "", ""]]
+
+	return {score, turn, placements}
+}
+
+function Player (gamepiece, game, board) {
+	const Move = (x, y) => {
+		if (game.placements[x][y] == ""){
+			game.placements[x][y] = gamepiece
+			board.Update()
+		} else {
+			Engine.Error("move")
+		}
+	}
+
+	return {gamepiece, game, board, Move}
+}
 
 function Board (game) {
 	const Update = () => {
