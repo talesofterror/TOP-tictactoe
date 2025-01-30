1c30f5fd3650f28abfac8e35fa1b0d54283dd59d
mike <mich.rivera84@gmail.com>
2025-01-29 21:07:56 -0500

    executing new tactic

--- script.js
+++ script.js
@@ -5,38 +5,69 @@ const Engine = (function () {
 	let turn = false
 
 	function Evaluate (target) {
-		for (row of target.game.placements) {
-			let rowIndex = target.game.placements.indexOf(row)
-			for (cell of row) {
-				if (cell == " ") {
-					continue
-				}
+		ResetScore(target)
+		let gamePlacements = target.game.placements
 
-				/*
-				 * score array: 
-				 * [0] = row 1, [1] = row 2, [2] = row 3
-				 * [3] = col 1, [4] = col 2, [5] = col 3
-				 * [6] = dia 1, [7] = dia 2
-				 *
-				 * array playerScore[length = 8]
-				 * array oppScore[length = 8]
-				 *
-				 * for row in gameboard
-				 * 		for cell in row
-				 * 			if cell == gamepiece
-				 * 			 increase score[row.index]
-				 * 			 increase score[2 + col.index]
-				 * 			 if (0, 0 || 2, 2)	
-				 * 			 	increase dia 1
-				 * 			 if (0, 2 || 2, 0)
-				 *				increase dia 2
-				 *
-				 * if cell in gameboard == 3
-				 * 	win
-				 */
+		for (let row = 0; row < gamePlacements.length; row ++) { // rows
+
+			for (let col = 0; col < gamePlacements[row].length; col++) {
+
+				if (gamePlacements[row][col] != " ") {
+					let playerPlacement = gamePlacements[row][col] 
+						== target.player.gamePiece ? true : false
+					let diagonalLeft = (row == 0 && col == 0) || (row == 2 && col == 2)
+					let diagonalRight = (row == 2 && col == 0) || (row == 0 && col == 2)
+					let centerPlacement = (row == 1 && col == 1)
+					if (playerPlacement) {
+						target.player.score[row]++
+						target.player.score[3 + col]++
+						if (diagonalLeft) {target.player.score[6]++}
+						if (diagonalRight) {target.player.score[7]++}
+						if (centerPlacement) { 
+							target.player.score[6]++
+							target.player.score[7]++
+						}
+					} else {
+
+					}
+				} else { continue }
 
 			}
+
 		}
+
+
+	/*
+	 * score array: 
+	 * [0] = row 1, [1] = row 2, [2] = row 3
+	 * [3] = col 1, [4] = col 2, [5] = col 3
+	 * [6] = dia L, [7] = dia R
+	 *
+	 * array playerScore[length = 8]
+	 * array oppScore[length = 8]
+	 *
+	 * score[all] = 0
+	 *
+	 * for row in gameboard
+	 * 		for cell in row
+	 * 			if cell == gamepiece
+	 * 			 increase score[row.index]
+	 * 			 increase score[3 + col.index]
+	 * 			 if (0, 0 || 2, 2)	
+	 * 			 	increase dia 1
+	 * 			 if (0, 2 || 2, 0)
+	 *				increase dia 2
+	 *			 if (1, 1)
+	 *			 	increase dia 1 & 2
+	 *
+	 * if score[any] == 3
+	 * 	win
+	 */
+
+	/*
+	 * for (let i = 0; i < target.game.placements)
+	 * */
+
 	}
 
 	// Consider Object.apply()
@@ -48,6 +79,11 @@ const Engine = (function () {
 		const newGame = {game, board, player, opponent}
 		gameArchive.push(newGame)
 		return newGame
+	}
+
+	function ResetScore (game) {
+		game.player.score.fill(0)
+		game.opponent.score.fill(0)
 	}
 
 	const Error = (errorType) => {
@@ -69,8 +105,7 @@ function Game () {
 }
 
 function Player (gamePiece, game, board) {
-	let scoreX = 0;
-	let scoreY = 0;
+	let score = new Array(8).fill(0);
 	const Move = (x, y) => {
 		if (game.placements[x][y] == " "){
 			game.placements[x][y] = gamePiece
@@ -80,7 +115,7 @@ function Player (gamePiece, game, board) {
 		}
 	}
 
-	return {gamePiece, game, board, scoreX, scoreY, Move}
+	return {gamePiece, game, board, score, Move}
 }
 
 function Board (game) {
