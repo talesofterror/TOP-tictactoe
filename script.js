dc8222bb610ce7cb9fdd392209bcde26cdd0ef0b
mike <mich.rivera84@gmail.com>
2025-03-03 23:20:13 -0500

    added css animation. enabled tie condition detection. am i done?

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-/* 
- * Need Tie game Condition
- *
- * Need to exclude occupied squares 
- * from opponent's random movement 
- * calculation for rare loop condition
- *
- */
 
 const Engine = (function () {
 	let gameArchive = []
 	let userTurn
 	let winState = false
+	let tieGame = 0
 	const gameboardElement = document.getElementById("gameboard")
-	// let cells
 	const gameDialogueElement = document.getElementById("game-dialogue")
 	const dialogues = {
 		"new-game": document.getElementById("new-game-dialogue"),
@@ -56,6 +48,9 @@ const Engine = (function () {
 			for (let j = 0; j < Engine.gameboardElement.children[i].children.length; j++) {
 				cellElements[i][j] = Engine.gameboardElement.children[i].children[j]
 				cellElements[i][j].textContent = "-"
+				cellElements[i][j].classList.remove("cell-user", "cell-computer", "no-click", 
+					"cell-user-placement", "cell-computer-placement",
+					"cell-user-win", "cell-computer-win")
 			}
 		}
 		
@@ -78,7 +73,7 @@ const Engine = (function () {
 		dialogues["new-game"].classList.add("invisible")
 		gameDialogueElement.style.zIndex = -1
 
-		TurnHandler(userTurn)
+		TurnHandler()
 
 		console.log(" ****** New game initiated! ******")
 
@@ -114,9 +109,21 @@ const Engine = (function () {
 		if (player.score.filter( (e) => e == 3 ).length != 0) {
 			console.log(player.score)
 			winState = true
-			WinGame(player)	
+			let winnerStyle = Engine.userTurn ? "cell-user-win" : "cell-computer-win"
+			player.placements.forEach((e) => e.classList.add(winnerStyle))
+			setTimeout(EndGame, 1000, player)
 		}
-		else { /* turn */ }
+		else { 
+			Engine.cells.forEach ((rCell) => {
+				rCell.forEach((cCell) => {
+					if (cCell.textContent == "x"){
+						Engine.tieGame++
+					}
+				})
+			})
+			if (Engine.tieGame == 5) { EndGame(player); Engine.winState = true } 
+			else { Engine.tieGame = 0 }
+		}
 	}
 
 	function TurnHandler () {
@@ -137,9 +144,16 @@ const Engine = (function () {
 		else { return }
 	}
 
-	function WinGame (player) {
+	function EndGame (player) {
+		const winnerElement = document.getElementById("winner")
+		if (Engine.tieGame == 5) {
+			winnerElement.textContent = "tie"
+		} else {
+			winnerElement.textContent = player.gamePiece 
+		}
 		DialogueHandler("result")
 		console.log(player.gamePiece + " wins the game!")
+		Engine.tieGame = 0
 	}
 
 	function GetCurrentGame () {
@@ -159,7 +173,7 @@ const Engine = (function () {
 	}
 
 	return {
-		gameArchive, gameboardElement, userTurn, winState,
+		gameArchive, gameboardElement, userTurn, winState, tieGame,
 		Evaluate, DialogueHandler, CreateGame, TurnHandler,
 		InitializeBoard, InitializeListeners,
 		Error, GetCurrentGame }
@@ -190,30 +204,44 @@ function GameBoard () {
 function Player(gamePiece) {
 
 	let score = new Array(8).fill(0)
+	let placements = []
 
 	function Move () {
 		const gameBoardTarget = Engine.GetCurrentGame().gameBoard
+		let userX = arguments[0]
+		let userY = arguments[1]
+		let rndX = Math.floor(Math.random() * 3)
+		let rndY = Math.floor(Math.random() * 3)
+
 		console.log (`userTurn on Move() call: ${Engine.userTurn}`)
 		if (Engine.userTurn){
-			console.log(`User move: ${arguments[0]}, ${arguments[1]}`)
-			if (gameBoardTarget.placements[arguments[0]][arguments[1]] == "-") {
-				gameBoardTarget.placements[arguments[0]][arguments[1]] = gamePiece
+			console.log(`User move: ${userX}, ${userY}`)
+			if (gameBoardTarget.placements[userX][userY] == "-") {
+				gameBoardTarget.placements[userX][userY] = gamePiece
+				placements.push(Engine.cells[userX][userY])
 			} else {
 				Engine.Error("move")
 				return
 			}
 		} else if (!Engine.userTurn) {
-				let rndX = Math.floor(Math.random() * 3)
-				let rndY = Math.floor(Math.random() * 3)
-
 				console.log(`Computer move: (${rndX}, ${rndY})`)
 
 				if (gameBoardTarget.placements[rndX][rndY] == "-") {
 					gameBoardTarget.placements[rndX][rndY] = gamePiece
+					placements.push(Engine.cells[rndX][rndY])
 				} else {
 					Engine.Error("move")
 					return
 				}
+		}
+
+		// animate placement
+		if (Engine.userTurn) {
+			Engine.cells[userX][userY].classList.add("cell-user-placement", "cell-user",
+				"no-click")
+		} else {
+			Engine.cells[rndX][rndY].classList.add("cell-computer-placement", "cell-computer",
+				"no-click")
 		}
 
 		let context = Engine.userTurn ? Engine.GetCurrentGame().player 
@@ -231,7 +259,7 @@ function Player(gamePiece) {
 	}
 
 	return {
-		gamePiece, score, 
+		gamePiece, score, placements,
 		Move, ResetScore 
 	}
 }
